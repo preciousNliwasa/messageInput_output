c4f9450fc8782fe1f12cf432af22e687a8253443
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-06-21 14:38:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from fastapi import FastAPI
-import pickle
+#import pickle
 
 app = FastAPI(title = 'Message Input/Output')
 
-vectorizer = pickle.load(open('vectorizer.pkl','rb'))
-model = pickle.load(open('chatbotModel.pkl','rb'))
+#vectorizer = pickle.load(open('vectorizer.pkl','rb'))
+#model = pickle.load(open('chatbotModel.pkl','rb'))
 
 @app.get('/',tags = ['Home'])
 async def home():
     return 'welcome'
 
-@app.get('/text_category',tags = ['Text Category'])
-async def text_category(message : str):
+#@app.get('/text_category',tags = ['Text Category'])
+#async def text_category(message : str):
     
-    message_transform = vectorizer.transform([message])
+#    message_transform = vectorizer.transform([message])
     
-    predicted = model.predict(message_transform)
+#    predicted = model.predict(message_transform)
     
-    return predicted[0]
+#    return predicted[0]
 
