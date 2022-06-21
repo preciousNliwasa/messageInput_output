04e00b79e2d4d3cfdef2e8c1859d831196261665
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-06-21 15:38:23 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,19 +3,19 @@ import joblib
 
 app = FastAPI(title = 'Message Input/Output')
 
-model = joblib.load('chatbot.joblib')
-vectorizer = joblib.load('vectorizer.joblib')
+#model = joblib.load('chatbot.joblib')
+#vectorizer = joblib.load('vectorizer.joblib')
 
 @app.get('/',tags = ['Home'])
 async def home():
     return 'welcome'
 
-@app.get('/text_category',tags = ['Text Category'])
-async def text_category(message : str):
+#@app.get('/text_category',tags = ['Text Category'])
+#async def text_category(message : str):
     
-    message_transform = vectorizer.transform([message])
+#    message_transform = vectorizer.transform([message])
     
-    predicted = model.predict(message_transform)
+#    predicted = model.predict(message_transform)
     
-    return predicted[0]
+#    return predicted[0]
 
