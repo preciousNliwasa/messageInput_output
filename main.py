0e0c9be029e9e1a7c03804df8e02c208d4cca8f0
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-06-21 12:31:12 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from fastapi import FastAPI
+import pickle
+
+
+app = FastAPI(title = 'Message Input/Output')
+
+vectorizer = pickle.load(open('vectorizer.pkl','rb'))
+model = pickle.load(open('chatbotModel.pkl','rb'))
+
+@app.get('/',tags = ['Home'])
+async def home():
+    
+    return 'welcome'
+
+@app.get('/text_category',tags = ['Text Category'])
+async def text_category(message : str):
+    
+    message_transform = vectorizer.transform([message])
+    
+    predicted = model.predict(message_transform)
+    
+    return predicted[0]
+
