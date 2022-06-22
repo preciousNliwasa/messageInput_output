e5edcae42968fa1f82f88882b7c19fb564496c22
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-06-22 09:41:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -11,6 +11,10 @@ GOOD_BOY_URL = (
     "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
 )
 
+@app.route("/")
+def home():
+    
+    return ('welcome')
 
 @app.route("/whatsapp", methods=["GET", "POST"])
 def reply_whatsapp():
