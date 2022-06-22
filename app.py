1d8669958716cb5109a1da2cb412a7091b2d5d43
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-06-22 08:51:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,48 +1,31 @@
-from flask import Flask,request
+from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
-import requests
-import numpy as np
-import pandas as pd
 
-app = Flask(__name__)                    
 
-@app.route("/")
-def home():
-  return ('hello')
+app = Flask(__name__)
 
-hh =  ''' @app.route("/whatsapp", methods=["GET", "POST"])
+
+GOOD_BOY_URL = (
+    "https://images.unsplash.com/photo-1518717758536-85ae29035b6d?ixlib=rb-1.2.1"
+    "&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+)
+
+
+@app.route("/whatsapp", methods=["GET", "POST"])
 def reply_whatsapp():
 
     try:
-        num_media = request.values.get("NumMedia")
-        inc = request.values.get("Body","").lower()
-        media = request.values.get('MediaContentType0', '')
-        phone_number = request.form.get('From')
-        
+        num_media = int(request.values.get("NumMedia"))
     except (ValueError, TypeError):
         return "Invalid request: invalid or missing NumMedia parameter", 400
     response = MessagingResponse()
-    
-    if 'hey' in inc:
-      
-      msg = response.message(inc)
-          
+    if not num_media:
+        msg = response.message("Send us an image!")
     else:
-        if media.startswith('image/'):
-            file_url = request.values['MediaUrl0']
-            msg = response.message('hey'))
-            msg.media(file_url)
-            
-        elif media.startswith('video/'):
-            file_url = request.values['MediaUrl0']
-            msg = response.message('you sent us this video')
-            msg.media(file_url)
-            
-        else:
-            msg = response.message('we dont understand what you have given bro')
-          
-    return str(response)'''
- 
-if __name__ == '__main__':
-  app.run()
+        msg = response.message("Thanks for the image. Here's one for you!")
+        msg.media(GOOD_BOY_URL)
+    return str(response)
 
+
+if __name__ == "__main__":
+    app.run()
