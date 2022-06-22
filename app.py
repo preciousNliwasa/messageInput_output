b7d8248fb3d894553fd77a9d128a96933ce32207
precious nliwasa <56281167+preciousNliwasa@users.noreply.github.com>
2022-06-22 08:19:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,6 @@ from flask import Flask,request
 from twilio.twiml.messaging_response import MessagingResponse
 import requests
 import numpy as np
-from io import BytesIO
-from PIL import Image
 import pandas as pd
 from skimage.transform import resize
 
@@ -18,11 +16,6 @@ bird = "https://49t059.deta.dev/stream/birdtensor.jpg"
 zuki = "https://49t059.deta.dev/stream/zuki.jpg"
 gh = "https://49t059.deta.dev/stream/gh.jpg"
 know_api= "https://r1lp8q.deta.dev/know/"
-
-def post_photo(url):
-  rr = requests.get(url)
-  img = Image.open(BytesIO(rr.content))
-  return str(type(img)) + 'was submitted'
 
 
 @app.route("/whatsapp", methods=["GET", "POST"])
@@ -45,7 +38,7 @@ def reply_whatsapp():
     else:
         if media.startswith('image/'):
             file_url = request.values['MediaUrl0']
-            msg = response.message(post_photo(file_url))
+            msg = response.message('hey'))
             msg.media(file_url)
             
         elif media.startswith('video/'):
